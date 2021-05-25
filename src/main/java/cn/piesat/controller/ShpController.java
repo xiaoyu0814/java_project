f0903392f292bf24c46a983537814c135af298e2
xiexiaoyu <xiexiaoyu@piesat.cn>
2021-05-25 17:58:38 +0800

    修改请求类型

--- src/main/java/cn/piesat/controller/ShpController.java
+++ src/main/java/cn/piesat/controller/ShpController.java
@@ -4,6 +4,7 @@ import cn.piesat.common.MyException;
 import cn.piesat.common.Result;
 import cn.piesat.pojo.AnalysisInfo;
 import cn.piesat.service.IShpService;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -39,14 +41,39 @@ public class ShpController {
 
     @RequestMapping(value = "/test",method = RequestMethod.GET)
     @ApiOperation("测试")
-    public Result test() {
+    // 简单方法
+    // public Result test(@RequestParam String a, String b) {
+    //     System.out.println(a);
+    //     System.out.println(b);
+    // };
+    // 参数为json字符串用此方法解析
+    public Result test(@RequestParam Map<String, Object> map) {
+
         Result result = new Result(0);
+
+        JSONObject resultJson=new JSONObject();
+
+        resultJson.put("name", "张三");
+        resultJson.put("age", 22);
+
         List<Object> objects = new ArrayList<>();
-        List<Object> obj = new ArrayList<>();
-        obj.add("啥也没有，气不气");
-        objects.add(obj);
-        result.setMessage("查询成功");
+        if( "1".equals(map.get("a").toString()) && "2".equals(map.get("b").toString())){
+            objects.add(resultJson);
+            result.setMessage("查询成功");
+        }else{
+            objects.add(null);
+            result.setMessage("查询失败");
+        }
+
         result.setData(objects);
+        return result;
+    }
+    @RequestMapping(value = "/testPost",method = RequestMethod.POST)
+    @ApiOperation("测试Post")
+    public Result testPost(@RequestParam Map<String, Object> map) {
+        Result result = new Result(0);
+        result.setData(map);
+        result.setMessage("查询成功");
         return result;
     }
 }
